e62917cad54984ad806179ac39d860f973ef582f
kalelb220 <kalelbryant895@gmail.com>
2025-10-14 09:19:10 -0400

    kalel

---
+++ javascript_functions.js
@@ -0,0 +1,25 @@
+//lab1
+ function calculateRectangleArea(length, width){
+            let area= length*width;
+    return area;
+}
+            function calculateCircleArea(radius){
+                return Math.PI*radius*radius;
+            }
+                console.log(calculateRectangleArea(5, 10)); // 50
+                console.log(calculateCircleArea(3)); // 28.27 (approx)
+
+                //Lab 2 
+function gradeCategory(score){
+    if >95 
+    return grade;
+}
+console.log(gradeCategory(95)); // "A"
+console.log(gradeCategory(72)); // "C"
+console.log(gradeCategory(58)); // "F"
+
+//task 3
+function  countVowels(word)
+return 
+
+console.log(countVowels("hello")); // 2
